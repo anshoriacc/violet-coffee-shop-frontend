3ea5d5f08dd948abb5423905e6ac17f216a1732c
achmad anshori <utd.rik@gmail.com>
2022-01-28 08:45:23 +0700

    fix naming file

---
+++ src/pages/404/NotFound.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "./notFound.scoped.css";
+
+export default class notFound extends Component {
+  render() {
+    return (
+      <>
+        <section class="page_404">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12 ">
+                <div class="col-sm-10 col-sm-offset-1  text-center banner">
+                  <div class="four_zero_four_bg">
+                    <h1 class="text-center ">404</h1>
+                  </div>
+                  <div class="contant_box_404">
+                    <h3 class="h2">Look like you're lost</h3>
+                    <p>the page you are looking for not avaible!</p>
+                    <a href="/" class="link_404">
+                      <button className="btn btn-warning">Go To Home</button>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  }
+}
---
+++ src/pages/SignUp/SignUp.js
@@ -0,0 +1,199 @@
+import React, { Component } from "react"
+import Footer from "../../components/Footer/Footer"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./SignUp.scoped.css"
+import logo from "../../assets/icons/logo.png"
+import iconGoogle from "../../assets/icons/google-logo-min.png"
+import imageLeft from "../../assets/images/background-loginregister.jpg"
+// import { Link } from "react-router-dom"
+
+export class SignUp extends Component {
+  render() {
+    return (
+      <>
+        <main className="container-fluid p-0">
+          <div className="row position-relative border">
+            <section className="col-lg-6 col-md-6 content-left">
+              <img
+                src={imageLeft}
+                alt="img-left"
+                className="img-fluid img-left"
+              />
+            </section>
+            <section className="col-lg-6 col-md-6 p-lg-4 content-right">
+              <div className="row">
+                <div className="col-lg-6 wrapper-title-coffee">
+                  <p className="title-coffee">
+                    <img
+                      src={logo}
+                      alt="logo-img"
+                      width={30}
+                      height={30}
+                      className="me-3"
+                    />
+                    Coffee Shop
+                  </p>
+                </div>
+                <div className="col-lg-6 col-md-6 justify-content-center d-flex">
+                  <button className="btn-signup">Login</button>
+                </div>
+                <div className="col-lg-12 col-md-6 text-center ps-md-5 mt-lg-5 mb-lg-3 wrapper-title-login">
+                  <p className="title-login">Sign Up</p>
+                </div>
+                <div className="col-lg-6 col-md-6 justify-content-center d-flex wrapper-btn-signup">
+                  <button className="btn-signup">Login</button>
+                </div>
+
+                <div className="col-lg-8 col-md-2 d-flex flex-column wrapper-form">
+                  <label className="label-form">Email Address :</label>
+                  <input
+                    typeof="email"
+                    className="input-form"
+                    placeholder="Enter your email address"
+                  />
+                  <label className="label-form">Password :</label>
+                  <input
+                    type="password"
+                    className="input-form"
+                    placeholder="Enter your password"
+                  />
+                  <label className="label-form">Phone Number :</label>
+                  <input
+                    typeof="email"
+                    className="input-form"
+                    placeholder="Enter your phone number"
+                  />
+
+                  <button className="btn-login">Sign Up</button>
+                  <button className="btn-google">
+                    <img
+                      src={iconGoogle}
+                      alt="icon-google"
+                      width={25}
+                      height={25}
+                      className="icon-google"
+                    />{" "}
+                    Sign Up with Google
+                  </button>
+                </div>
+              </div>
+            </section>
+            <div className="row card" id="card">
+              <div className="col-9 wrapper-title-card">
+                <div className="row">
+                  <div className="col-lg-5 wrapper-title ms-5">
+                    <h1 className="title-card">Get your member card now!</h1>
+                  </div>
+                </div>
+                <p className="main-card ps-5">
+                  Let's join with our member and enjoy the deals.
+                </p>
+              </div>
+              <div className="col-lg-3 col-md-5 ps-md-5 ps-lg-0">
+                <button className="create-now">Create Now</button>
+              </div>
+            </div>
+          </div>
+          <Footer />
+        </main>
+      </>
+    )
+  }
+}
+
+export default SignUp
+
+// import React, { Component } from "react"
+// import "./SignUp.scoped.css"
+
+// import banner from "../../assets/images/login-signup.jpeg"
+// import logo from "../../assets/icons/logo.png"
+// import google from "../../assets/icons/google-logo-min.png"
+
+// // components
+// import Footer from "../../components/Footer/Footer"
+
+// export default class index extends Component {
+//   render() {
+//     return (
+//       <>
+//         <div className="main">
+//           <div className="forms">
+//             <div className="left-container">
+//               <img src={banner} alt="" />
+//             </div>
+//             <div className="right-container">
+//               <div className="logo">
+//                 <img src={logo} alt="" />
+//                 <p>Coffee Shop</p>
+//               </div>
+//               <div className="button">
+//                 <a href="/">
+//                   <button className="btn btn-warning">Login</button>
+//                 </a>
+//               </div>
+//               <h1 className="tittle">Sign Up</h1>
+//               <form>
+//                 <div class="mb-3">
+//                   <label for="exampleInputEmail1" class="form-label">
+//                     Email address :
+//                   </label>
+//                   <input
+//                     type="email"
+//                     class="form-control"
+//                     id="exampleInputEmail1"
+//                     aria-describedby="emailHelp"
+//                     placeholder="Enter your email address"
+//                   />
+//                 </div>
+//                 <div class="mb-3">
+//                   <label for="exampleInputPassword1" class="form-label">
+//                     Password :
+//                   </label>
+//                   <input
+//                     type="password"
+//                     class="form-control"
+//                     id="exampleInputPassword1"
+//                     placeholder="Enter your password"
+//                   />
+//                 </div>
+//                 <div class="mb-3">
+//                   <label for="exampleInputPassword1" class="form-label">
+//                     Phone Number :
+//                   </label>
+//                   <input
+//                     type="password"
+//                     class="form-control"
+//                     id="exampleInputPassword1"
+//                     placeholder="Enter your Phone number"
+//                   />
+//                 </div>
+//               </form>
+//               <div className="button-container">
+//                 <button className="btn btn-warning">Sign Up</button>
+//                 <button className="btn btn-light">
+//                   <img src={google} alt="" /> Sign up with Google
+//                 </button>
+//               </div>
+//             </div>
+//           </div>
+//           <div className="fly">
+//             <div className="text">
+//               <h2>
+//                 Get your member <br /> card now!
+//               </h2>
+//               <p>Let's join with our member and enjoy the deals.</p>
+//             </div>
+//             <div className="create">
+//               <a href="/">
+//                 <button className="btn btn-warning ">Create now</button>
+//               </a>
+//             </div>
+//           </div>
+//           {/* <Footer className="footer" /> */}
+//         </div>
+//         <Footer />
+//       </>
+//     )
+//   }
+// }
