1ecc9931d84376d52e01427c3c9fa4a3b63b68d7
Fahrul Muhammad <fahrulmuh23@gmail.com>
2022-03-19 18:13:00 +0800

    add redux delivery items perbaikan styling dan implementasi di halaman product detail dan perbaiki styling di halaman payment

--- src/Redux/reducers/index.js
+++ src/Redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./auth";
+import deliveryReducer from "./delivery";
 
 const reducers = combineReducers({
   auth: authReducer,
+  delivery: deliveryReducer,
 });
 
 export default reducers;
--- src/pages/Payment&Delivery/Payment.js
+++ src/pages/Payment&Delivery/Payment.js
@@ -3,14 +3,17 @@ import "./Payment.scoped.css";
 
 import Navbar from "../../components/Navbar/NavLog";
 import Footer from "../../components/Footer/Footer";
+import { connect } from "react-redux";
+import { formater } from "../../helpers/formatNumber";
 
 import Image from "../../assets/images/background-profile.jpg";
 
-export default class Payment extends Component {
+class Payment extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isEdit: false,
+      totalPrice: 0,
     };
   }
 
@@ -18,6 +21,20 @@ export default class Payment extends Component {
     this.setState({ isEdit: !this.state.isEdit });
     console.log(this.state.isEdit);
   };
+
+  subTotal = () => {
+    let total = 0;
+    for (let i = 0; i < this.props.delivery.length; i++) {
+      total += this.props.delivery[i].totalPrice;
+    }
+    console.log("TOTAL BRO", total);
+    this.setState({ totalPrice: total });
+  };
+
+  componentDidMount() {
+    this.subTotal();
+    console.log("USER DATA", this.props.users);
+  }
 
   render() {
     return (
@@ -29,24 +46,30 @@ export default class Payment extends Component {
           </p>
           <div className="order-card">
             <p className="order-summary">Order summary</p>
-            <div className="card-item">
+            {this.props.delivery.map((val) => {
+              return (
+                <div className="card-item">
+                  <img src={Image} alt="" className="product-image" />
+                  <div>
+                    <p className="product-name">{val.name}</p>
+                    <p className="product-order">x{val.count}</p>
+                    <p className="size-order">{val.size}</p>
+                    <p className="product-price">{formater.format(val.totalPrice)}</p>
+                  </div>
+                </div>
+              );
+            })}
+            {/* <div className="card-item">
               <img src={Image} alt="" className="product-image" />
               <p className="product-name">Caffee Latte</p>
               <p className="product-order">x1</p>
               <p className="size-order">Regular</p>
               <p className="product-price">IDR 25.000</p>
-            </div>
-            <div className="cards">
-              <img src={Image} alt="" className="product-image" />
-              <p className="product-name">Caffee Latte</p>
-              <p className="product-order">x1</p>
-              <p className="size-order">Regular</p>
-              <p className="product-price">IDR 25.000</p>
-            </div>
+            </div> */}
             <div className="order">
               <div className="subtotal">
                 <p className="sub-total">SUBTOTAL</p>
-                <p className="sub-idr">IDR 50.000</p>
+                <p className="sub-idr">{formater.format(this.state.totalPrice)}</p>
               </div>
               <div className="tax-fees">
                 <p className="tax">TAX & FEES</p>
@@ -58,7 +81,7 @@ export default class Payment extends Component {
               </div>
               <div className="total">
                 <p className="total-order">TOTAL</p>
-                <p className="total-idr">IDR 65.000</p>
+                <p className="total-idr">{formater.format(this.state.totalPrice + 5000 + 10000)}</p>
               </div>
             </div>
           </div>
@@ -72,13 +95,13 @@ export default class Payment extends Component {
             <div className="address-card">
               <form>
                 <div class="mb-0">
-                  <input type="email" class="form-control shadow-none" id="exampleInputEmail1" aria-describedby="emailHelp" disabled={this.state.isEdit} />
+                  <input type="email" class="form-control shadow-none" placeholder={`Delivery To ${this.props.users.delivery_adress}`} id="exampleInputEmail1" aria-describedby="emailHelp" disabled={this.state.isEdit} />
                 </div>
                 <div class="mb-0">
-                  <input type="text" class="form-control shadow-none" id="exampleInputPassword1" disabled={this.state.isEdit} />
+                  <input type="text" class="form-control shadow-none" placeholder={this.props.users.delivery_adress} id="exampleInputPassword1" disabled={this.state.isEdit} />
                 </div>
                 <div class="mb-0">
-                  <input type="text" class="form-control shadow-none" id="exampleInputPassword1" disabled={this.state.isEdit} />
+                  <input type="text" class="form-control shadow-none" placeholder={`+62 ${this.props.users.phone}`} id="exampleInputPassword1" disabled={this.state.isEdit} />
                 </div>
               </form>
             </div>
@@ -96,3 +119,13 @@ export default class Payment extends Component {
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    delivery: state.delivery.item,
+    users: state.auth.userData,
+    token: state.auth.token,
+  };
+};
+
+export default connect(mapStateToProps)(Payment);
