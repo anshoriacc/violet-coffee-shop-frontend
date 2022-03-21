06f5a74da95e0f46d912809cfcead79dd3e05365
achmad anshori <utd.rik@gmail.com>
2022-03-21 14:03:21 +0700

    implement history

--- src/pages/History/History.js
+++ src/pages/History/History.js
@@ -1,43 +1,60 @@
-import React, { Component } from "react";
-import "./History.scoped.css";
+import React, {Component} from 'react';
+import './History.scoped.css';
+import {Modal} from 'react-bootstrap';
 
 // COMPONENTS
-import Header from "../../components/Navbar/Navbar";
-import Footer from "../../components/Footer/Footer";
-import CardHistory from "../../components/CardHistory/CardHistory";
+import Header from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
+import CardHistory from '../../components/CardHistory/CardHistory';
 
-import Dumy from "../../assets/images/background-profile.jpg";
+import Dumy from '../../assets/images/background-profile.jpg';
 
 // import { Link } from "react-router-dom";
-import { GetHistory } from "../../utils/history";
-import { connect } from "react-redux";
+import {deleteHistory, getHistory} from '../../utils/history';
+import {connect} from 'react-redux';
 
 class History extends Component {
   constructor(props) {
     super(props);
     this.state = {
       userHistory: [],
-      id: [],
+      id: 0,
+      modalVisibility: false,
     };
-    this.inputFile = React.createRef();
+    // this.inputFile = React.createRef();
   }
 
-  fromChange = (e) => {
-    const ids = [...this.state.id];
-    ids.push(e.target.value);
-    this.setState({ id: ids }, () => {
-      console.log(this.state.id);
-    });
+  // formChange = (id) => {
+  //   const ids = [...this.state.id];
+  //   ids.push(id);
+  //   this.setState({id: ids}, () => {
+  //     console.log(this.state.id);
+  //   });
+  // };
+
+  deletePrompt = () => {
+    deleteHistory(this.props.token, this.state.id)
+      .then((res) => {
+        // console.log(res);
+        let x = document.getElementById('snackbar');
+        x.className = 'show';
+        setTimeout(function() {
+          x.className = x.className.replace('show', '');
+        }, 3000);
+      })
+      .catch((err) => {
+        console.log({err});
+      });
   };
 
   getHistoryUser = () => {
     const token = this.props.token;
-    GetHistory(token)
+    getHistory(token)
       .then((res) => {
-        this.setState({ userHistory: res.data.data });
+        this.setState({userHistory: res.data.data});
       })
       .catch((err) => {
-        console.log(err);
+        console.log({err});
       });
   };
 
@@ -53,18 +70,62 @@ class History extends Component {
         <div className="background">
           <div className="content">
             <div className="section-title">
-              <h1>Let's see what you have bought!</h1>
-              <p>Select item to delete</p>
+              {this.state.userHistory.length > 0 ? (
+                <>
+                  <h1>Let's see what you have bought!</h1>
+                  <p>Select item to delete</p>
+                </>
+              ) : (
+                <>
+                  <h1>No history found. start order one!</h1>
+                </>
+              )}
             </div>
             <div className="card-container">
               {userHistory.map((val) => {
                 console.log(val.name);
-                return <CardHistory image={Dumy} name={val.name} price={val.price} id={val.id} key={val.id} />;
+                return (
+                  <CardHistory
+                    image={Dumy}
+                    name={val.name}
+                    price={val.price}
+                    id={val.id}
+                    key={val.id}
+                    onClick={() => {
+                      this.setState({id: val.id});
+                      this.setState({modalVisibility: true});
+                    }}
+                  />
+                );
               })}
             </div>
           </div>
         </div>
         <Footer />
+        {/* MODAL */}
+        <Modal show={this.state.modalVisibility} centered>
+          <Modal.Body className="modal-body">
+            <p className="modal-text">
+              Are you sure want to delete this transaction?
+            </p>
+          </Modal.Body>
+          <Modal.Footer className="modal-footer">
+            <div className="modal-btn">
+              <button className="btn btn-warning" onClick={this.deletePrompt}>
+                Yes
+              </button>
+              <button
+                className="btn btn-light"
+                onClick={() =>
+                  this.setState({modalVisibility: !this.state.modalVisibility})
+                }
+              >
+                No
+              </button>
+            </div>
+          </Modal.Footer>
+        </Modal>
+        <div id="snackbar">Berhasil delete transaction</div>
       </>
     );
   }
--- src/utils/history.js
+++ src/utils/history.js
@@ -1,6 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const GetHistory = (accessToken) => {
-  const URL = `${process.env.REACT_APP_HOST}/payment/payment-userId?per_page=15&page=1`;
-  return axios.get(URL, { headers: { "x-access-token": accessToken } });
+export const getHistory = (accessToken) => {
+  const URL = `${
+    process.env.REACT_APP_HOST
+  }/payment/payment-userId?per_page=15&page=1`;
+  return axios.get(URL, {headers: {'x-access-token': accessToken}});
+};
+
+export const deleteHistory = (accessToken, id) => {
+  const URL = `${process.env.REACT_APP_HOST}/payment/${id}`;
+  return axios.delete(URL, {headers: {'x-access-token': accessToken}});
 };
